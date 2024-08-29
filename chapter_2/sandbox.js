e2fb484a3dd0343c76b427be518f553ce31690d5
llaquette2 <llaquette2@academic.rrc.ca>
2024-08-29 15:36:17 -0500

    Updated sandbox.js with template strings practice
    Learning about template strings/literals.

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -74,3 +74,25 @@ let likes = 10;
 
 let result = 'the blog has ' + likes + ' likes';
 console.log(result); */
+
+// TEMPLATE STRINGS
+const title = 'Best reads of 2024';
+const author = 'Landon';
+const likes = 30;
+
+// concatenation way
+// let result = 'The blog called ' + title + ' by ' + author + ' has ' + likes + ' likes';
+// console.log(result);
+
+// template string way
+// let result = `The blog called ${title} by ${author} has ${likes} likes`;
+// console.log(result);
+
+// creating html templates
+let html = `
+    <h2>${title}</h2>
+    <p>By ${author}</p>
+    <span>This blog has ${likes} likes</span>
+`;
+
+console.log(html);
