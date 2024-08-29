e16c607ddfd41a874fb3601d98c045c01c2b8406
llaquette2 <llaquette2@academic.rrc.ca>
2024-08-29 15:16:58 -0500

    Updated sandbox.js with string methods
    Learning about the methods used with strings.

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,4 +1,4 @@
-// strings
+/* // strings
 console.log('hello, world');
 
 let email = 'mario@thenetninja.co.uk';
@@ -23,4 +23,20 @@ let result = fullName.toLowerCase();
 console.log(result, fullName);
 
 let index = email.indexOf('@');
-console.log(index);
+console.log(index); */
+
+// common string methods
+
+let email = 'mario@thenetninja.co.uk';
+
+// let result = email.lastIndexOf('n');
+
+// let result = email.slice(2, 5);
+
+// let result = email.substr(4, 10);
+
+// let result = email.replace('m', 'w');
+
+// let result = email.replace('n', 'w');
+
+console.log(result);
