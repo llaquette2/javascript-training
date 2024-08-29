68564d1139156ba1ef9e41188ac4b457d9db65f9
llaquette2 <llaquette2@academic.rrc.ca>
2024-08-29 15:45:57 -0500

    Updated sandbox.js with arrays practice
    Learning about using arrays in JavaScript.

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -73,7 +73,7 @@ let likes = 10;
 // console.log(5 / 'hi');
 
 let result = 'the blog has ' + likes + ' likes';
-console.log(result); */
+console.log(result);
 
 // TEMPLATE STRINGS
 const title = 'Best reads of 2024';
@@ -95,4 +95,28 @@ let html = `
     <span>This blog has ${likes} likes</span>
 `;
 
-console.log(html);
+console.log(html); */
+
+// ARRAYS
+let ninjas = ['landon', 'ryu', 'chun-li'];
+
+// ninjas[1] = 'ken';
+// console.log(ninjas[1]);
+
+// let ages = [20, 25, 30, 35];
+// console.log(ages[2]);
+
+// let random = ['landon', 'crystal', 30, 20];
+// console.log(random);
+
+// console.log(ninjas.length);
+
+// array methods
+
+// let result = ninjas.join(',');
+// let result = ninjas.indexOf('chun-li');
+// let result = ninjas.concat('ken', 'crystal');
+let result = ninjas.push('ken');
+result = ninjas.pop();
+
+console.log(result);
