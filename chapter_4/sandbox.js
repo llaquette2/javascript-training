51c832620369e20384900b43820213be0f848f6a
llaquette2 <llaquette2@academic.rrc.ca>
2024-08-30 15:31:50 -0500

    Updated index.html and sandbox.js
    Practice with methods & functions, callback functions & forEach method, and referencing the html and using html templates.

--- chapter_4/sandbox.js
+++ chapter_4/sandbox.js
@@ -34,7 +34,7 @@ const calcArea = function(radius) {
 
 const area = calcArea(5);
 console.log(area);
-*/
+
 // regular function
 // const calcArea = function(radius) {
 //     return 3.14 * radius**2;
@@ -69,3 +69,59 @@ const bill = (products, tax) => {
 };
 
 console.log(bill([10,15,30], 0.2));
+
+// methods and functions
+const name = 'landon';
+
+// functions
+
+const greet = () => 'hello';
+
+let resultOne = greet();
+console.log(resultOne);
+
+// methods
+
+let resultTwo = name.toUpperCase();
+console.log(resultTwo);
+
+// callbacks & foreach
+
+// const myFunc = (callbackFunc) => {
+//     // do something
+//     let value = 50;
+//     callbackFunc(value);
+// };
+
+// myFunc(value => {
+//     // do something
+//     console.log(value);
+// });
+
+let people = ['mario', 'luigi', 'ryu', 'landon', 'chun-li'];
+
+
+const logPerson = (person, index) => {
+    console.log(`${index} - hello ${person}!`);
+};
+
+people.forEach(logPerson);
+
+// people.forEach((person, index) => {
+//     console.log(index, person);
+// });
+*/
+// get a reference to the 'ul'
+const ul = document.querySelector('.people');
+
+const people = ['mario', 'luigi', 'ryu', 'landon', 'chun-li'];
+
+let html = ``;
+
+people.forEach(person => {
+    // create html template
+    html += `<li style="color: purple">${person}</li>`;
+});
+
+console.log(html);
+ul.innerHTML = html;
