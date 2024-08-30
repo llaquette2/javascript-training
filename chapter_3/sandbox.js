0b5e478ee04d02b7d4f6d5ae2d1558e8fa4b70d3
llaquette2 <llaquette2@academic.rrc.ca>
2024-08-30 10:53:52 -0500

    Created index.html and sandbox.js, practiced for loops, while loops and do while loops

---
+++ chapter_3/sandbox.js
@@ -0,0 +1,38 @@
+// FOR LOOPS
+/*
+// for(let i = 0; i < 5; i++) {
+//     console.log('in loop:', i);
+// }
+
+// console.log('loop finished');
+
+const names = ['landon', 'mario', 'luigi'];
+
+for(let i = 0; i < names.length; i++) {
+    // console.log(names[i]);
+    let html = `<div>${names[i]}</div>`;
+    console.log(html);
+}
+
+// WHILE LOOPS
+const names = ['landon', 'mario', 'luigi'];
+// let i = 0;
+
+// while(i < 5) {
+//     console.log('in loop:', i);
+//     i++;
+// }
+
+let i = 0;
+while(i < names.length) {
+    console.log(names[i]);
+    i++;
+}
+*/
+// DO WHILE LOOPS
+let i = 3;
+
+do {
+    console.log('val of i is: ', i);
+    i++;
+} while(i < 5);
