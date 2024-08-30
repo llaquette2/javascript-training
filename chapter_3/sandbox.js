41382b0de003e9ca007c20d94c70abb5a3dad644
llaquette2 <llaquette2@academic.rrc.ca>
2024-08-30 11:24:18 -0500

    Updated sandbox.js with practice on conditional statements, logical operators, and the break and continue keywords

--- chapter_3/sandbox.js
+++ chapter_3/sandbox.js
@@ -1,5 +1,6 @@
 // FOR LOOPS
 /*
+// normal for loops
 // for(let i = 0; i < 5; i++) {
 //     console.log('in loop:', i);
 // }
@@ -14,7 +15,7 @@ for(let i = 0; i < names.length; i++) {
     console.log(html);
 }
 
-// WHILE LOOPS
+// while loops
 const names = ['landon', 'mario', 'luigi'];
 // let i = 0;
 
@@ -28,11 +29,85 @@ while(i < names.length) {
     console.log(names[i]);
     i++;
 }
-*/
-// DO WHILE LOOPS
+
+// do while loops
 let i = 3;
 
 do {
     console.log('val of i is: ', i);
     i++;
 } while(i < 5);
+
+// CONDITIONAL STATEMENTS
+
+// if statements
+// const age = 25;
+
+// if(age > 20){
+//     console.log('you are over 20 years old');
+// }
+
+// const ninjas = ['landon', 'ryu', 'chun-li', 'yoshi'];
+
+// if(ninjas.length > 4) {
+//     console.log("that's a lot of ninjas");
+// }
+
+const password = 'p@ssword';
+
+if(password.length >= 8) {
+    console.log('that password is long enough!');
+}
+
+// else if statements
+
+const password = 'p@ss';
+
+if(password.length >= 12) {
+    console.log('that password is mighty strong');
+} else if(password.length >= 8) {
+    console.log('that password is long enough!');
+} else {
+    console.log('password is not long enough');
+}
+
+// logical operators - OR || and AND &&
+
+const password = 'p@ss12';
+
+if(password.length >= 12 && password.includes('@')) {
+    console.log('that password is mighty strong');
+} else if(password.length >= 8 || password.includes('@') && password.length > 5) {
+    console.log('that password is strong enough!');
+} else {
+    console.log('password is not strong enough');
+}
+
+// logical NOT (!)
+
+let user = false;
+
+if(!user) {
+    console.log('you must be logged in to continue');
+}
+
+console.log(!true);
+console.log(!false);
+*/
+// break and continue
+
+const scores = [50, 25, 0, 30, 100, 20, 10];
+
+for(let i = 0; i < scores.length; i++) {
+
+    if(scores[i] === 0) {
+        continue;
+    }
+
+    console.log('your score: ', scores[i]);
+
+    if(scores[i] === 100) {
+        console.log('congrats, you got the top score!');
+        break;
+    }
+}
