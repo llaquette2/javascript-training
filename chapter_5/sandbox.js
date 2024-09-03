ce78af66bf44ad66c8e0712b14e11e36ca6e9a8a
llaquette2 <llaquette2@academic.rrc.ca>
2024-09-03 13:23:04 -0500

    Updated sandbox.js
    Updated with training on the Math object, random numbers, and the difference between primitive values and reference values.

--- chapter_5/sandbox.js
+++ chapter_5/sandbox.js
@@ -1,5 +1,5 @@
 // object literals
-
+/*
 // const blogs = [
 //     { title: 'why mac & cheese rules', likes: 30 },
 //     { title: '10 things to make with marmite', likes: 50 }
@@ -51,3 +51,44 @@ blogs: [
 // user.logout();
 
 user.logBlogs();
+
+// Math object
+
+console.log(Math);
+console.log(Math.PI);
+console.log(Math.E);
+
+const area = 7.7;
+
+console.log(Math.round(area));
+console.log(Math.floor(area));
+console.log(Math.ceil(area));
+console.log(Math.trunc(area));
+
+// random numbers
+
+const random = Math.random();
+
+console.log(random);
+console.log(Math.round(random * 100));
+*/
+
+// primitive values
+
+// let scoreOne = 50;
+// let scoreTwo = scoreOne;
+
+// console.log(`scoreOne: ${scoreOne}`, `scoreTwo: ${scoreTwo}`);
+
+// scoreOne = 100;
+// console.log(`scoreOne: ${scoreOne}`, `scoreTwo: ${scoreTwo}`);
+
+// reference values
+
+const userOne = { name: 'ryu', age: 30 };
+const userTwo = userOne;
+
+console.log(userOne, userTwo);
+
+userOne.name = 'chun-li';
+console.log(userOne, userTwo);
