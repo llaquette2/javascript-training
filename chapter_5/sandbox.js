5b1fd8993ac050e7436bc1eb9288ef5109b592c5
llaquette2 <llaquette2@academic.rrc.ca>
2024-09-03 11:42:04 -0500

    Updated sandbox.js
    Updated with more training on object literals, like use of 'this' keyword and arrays of objects.

--- chapter_5/sandbox.js
+++ chapter_5/sandbox.js
@@ -1,25 +1,53 @@
 // object literals
 
+// const blogs = [
+//     { title: 'why mac & cheese rules', likes: 30 },
+//     { title: '10 things to make with marmite', likes: 50 }
+// ];
+
+// console.log(blogs);
+
 let user = {
     name: 'crystal',
     age: 30,
     email: 'crystal@thenetninja.co.uk',
     location: 'berlin',
-    blogs: ['why mac & cheese rules', '10 things to make with marmite']
+blogs: [
+        { title: 'why mac & cheese rules', likes: 30 },
+        { title: '10 things to make with marmite', likes: 50 }
+    ],
+    login() {
+        console.log('the user logged in');
+    },
+    logout() {
+        console.log('the user logged out');
+    },
+    logBlogs() {
+        //console.log(this.blogs);
+        console.log('this user has written the following blogs:');
+        this.blogs.forEach(blog => {
+            console.log(blog.title, blog.likes);
+        });
+    }
 };
 
-console.log(user);
-console.log(user.name);
+// console.log(user);
+// console.log(user.name);
 
 //user.age = 35;
-console.log(user.age);
+// console.log(user.age);
 
 // const key = 'location';
 
 // console.log(user[key]);
 
-console.log(user['location']);
-user['name'] = 'chun-li';
-console.log(user['name']);
+// console.log(user['location']);
+// user['name'] = 'chun-li';
+// console.log(user['name']);
 
-console.log(typeof user);
+// console.log(typeof user);
+
+// user.login();
+// user.logout();
+
+user.logBlogs();
